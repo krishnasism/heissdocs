4e230cb487539e514c44ec04693dd8113d876e90
Krishnasis Mandal <krishnasismandal97@gmail.com>
2023-05-11 00:34:48 +0200

    Add logo

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -3,7 +3,8 @@
         <nav class="bg-white dark:bg-gray-900 fixed w-full z-50 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="/" class="flex items-center">
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">heißdocs</span>
+                    <img src="/public/logo/logo.png" height="20" width="100" alt="heißdocs">
+                    <!-- <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">heißdocs</span> -->
                 </a>
                 <div class="flex md:order-2">
                     <LanguageSwitcher class="mr-4"></LanguageSwitcher>
