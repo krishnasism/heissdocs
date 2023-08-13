718191d37ec1290f7656934e1b36e6e9b3078d57
Krishnasis Mandal <krishnasismandal97@gmail.com>
2023-08-13 23:23:41 +0200

    Remove text from buttons

--- frontend/src/components/AskInput.vue
+++ frontend/src/components/AskInput.vue
@@ -13,8 +13,12 @@
                 class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 :placeholder="$t('labels.ask')" required>
             <button @click="submitAsk"
-                class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{{
-                    $t('component.ask') }}</button>
+                class="bg-gray-200 text-white absolute right-2.5 bottom-2.5 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <svg class="w-4 h-4 text-gray-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                    fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m1 9 4-4-4-4" />
+                </svg></button>
         </div>
     </div>
 </template>
--- frontend/src/components/SearchInput.vue
+++ frontend/src/components/SearchInput.vue
@@ -13,7 +13,12 @@
                 class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 :placeholder="$t('component.search')" required>
             <button @click="submitSearch"
-                class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{{ $t('labels.search') }}</button>
+                class="bg-gray-200 text-white absolute right-2.5 bottom-2.5 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><svg
+                    class="w-4 h-4 text-gray-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                    fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                </svg></button>
         </div>
         <div class="flex mt-2">
             <div class="flex items-center h-5">
@@ -22,8 +27,10 @@
                     class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
             </div>
             <div class="ml-2 text-sm mr-2">
-                <label for="chkSearchElastic" class="font-medium text-gray-900 dark:text-gray-300">{{ $t('labels.searchElasticsearch') }}</label>
-                <p id="chkSearchElastic-text" class="text-xs font-normal text-gray-500 dark:text-gray-300">{{ $t('labels.searchElasticsearchHelper') }}</p>
+                <label for="chkSearchElastic" class="font-medium text-gray-900 dark:text-gray-300">{{
+                    $t('labels.searchElasticsearch') }}</label>
+                <p id="chkSearchElastic-text" class="text-xs font-normal text-gray-500 dark:text-gray-300">{{
+                    $t('labels.searchElasticsearchHelper') }}</p>
             </div>
             <div class="flex items-center h-5 mr-2">
                 <input id="chkSearchDocumentDb" aria-describedby="chkSearchDocumentDb-text" type="checkbox"
@@ -31,8 +38,10 @@
                     class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
             </div>
             <div class="ml-2 text-sm">
-                <label for="chkSearchDocumentDb" class="font-medium text-gray-900 dark:text-gray-300">{{ $t('labels.searchDocumentDb') }}</label>
-                <p id="chkSearchDocumentDb-text" class="text-xs font-normal text-gray-500 dark:text-gray-300">{{ $t('labels.searchDocumentDbHelper') }}</p>
+                <label for="chkSearchDocumentDb" class="font-medium text-gray-900 dark:text-gray-300">{{
+                    $t('labels.searchDocumentDb') }}</label>
+                <p id="chkSearchDocumentDb-text" class="text-xs font-normal text-gray-500 dark:text-gray-300">{{
+                    $t('labels.searchDocumentDbHelper') }}</p>
             </div>
         </div>
     </div>
