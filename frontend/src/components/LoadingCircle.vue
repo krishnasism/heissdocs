d91e93a4233a4d6b8684222bc57aab25866b220c
Krishnasis Mandal <krishnasismandal97@gmail.com>
2023-08-19 19:44:16 +0200

    Add visual feedback for refresh

--- frontend/src/components/LoadingCircle.vue
+++ frontend/src/components/LoadingCircle.vue
@@ -1,14 +1,37 @@
 <template>
     <div class="loading-circle">
-        <div class="circle"></div>
+        <button @click="refreshPage" v-if="showRefreshMessage">
+            <svg class="w-8 h-8 text-gray-600 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                fill="none" viewBox="0 0 18 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M16 1v5h-5M2 19v-5h5m10-4a8 8 0 0 1-14.947 3.97M1 10a8 8 0 0 1 14.947-3.97" />
+            </svg>
+        </button>
+        <div class="circle" v-else></div>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'LoadingCircle',
-}
+    name: 'LoadingCircle',
+    data() {
+        return {
+            showRefreshMessage: false,
+        };
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showRefreshMessage = true;
+        }, 30000);  //30s
+    },
+    methods: {
+        refreshPage() {
+            location.reload();
+        },
+    },
+};
 </script>
+
 
 <style>
 .loading-circle {
