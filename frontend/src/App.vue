175f835e534bdbf11595efbd47ef81d862886482
Krishnasis Mandal <krishnasismandal97@gmail.com>
2023-08-24 22:29:19 +0200

    uniform ui

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -25,7 +25,7 @@
                 'flex items-center p-2 text-gray-600 rounded-lg dark:text-white hover:bg-blue-100 dark:hover:bg-gray-700': true
               }">
                 <svg fill="currentColor"
-                  class="w-6 h-6 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                  class="w-6 h-6  dark:text-gray-400 dark:group-hover:text-white"
                   viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                   <path
                     d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z">
@@ -56,7 +56,7 @@
                 'flex items-center p-2 text-gray-600 rounded-lg dark:text-white hover:bg-blue-100 dark:hover:bg-gray-700': true
               }">
                 <svg
-                  class="flex-shrink-0 w-6 h-6 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                  class="flex-shrink-0 w-6 h-6  dark:text-gray-400 dark:group-hover:text-white"
                   fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M16 5h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1h-2v3l-4-3H8m4-13H2a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h2v3l4-3h4a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z" />
@@ -66,10 +66,10 @@
             </li>
             <li>
               <button type="button"
-                class="flex items-center w-full p-2 text-base text-gray-900 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                class="flex items-center w-full p-2 text-base text-gray-600 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                 aria-controls="cloud-operations" data-collapse-toggle="cloud-operations">
                 <svg
-                  class="flex-shrink-0 w-6 h-6 text-gray-500 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                  class="flex-shrink-0 w-6 h-6 text-gray-500 dark:text-gray-400 dark:group-hover:text-white"
                   fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                   <path clip-rule="evenodd" fill-rule="evenodd"
                     d="M.99 5.24A2.25 2.25 0 013.25 3h13.5A2.25 2.25 0 0119 5.25l.01 9.5A2.25 2.25 0 0116.76 17H3.26A2.267 2.267 0 011 14.74l-.01-9.5zm8.26 9.52v-.625a.75.75 0 00-.75-.75H3.25a.75.75 0 00-.75.75v.615c0 .414.336.75.75.75h5.373a.75.75 0 00.627-.74zm1.5 0a.75.75 0 00.627.74h5.373a.75.75 0 00.75-.75v-.615a.75.75 0 00-.75-.75H11.5a.75.75 0 00-.75.75v.625zm6.75-3.63v-.625a.75.75 0 00-.75-.75H11.5a.75.75 0 00-.75.75v.625c0 .414.336.75.75.75h5.25a.75.75 0 00.75-.75zm-8.25 0v-.625a.75.75 0 00-.75-.75H3.25a.75.75 0 00-.75.75v.625c0 .414.336.75.75.75H8.5a.75.75 0 00.75-.75zM17.5 7.5v-.625a.75.75 0 00-.75-.75H11.5a.75.75 0 00-.75.75V7.5c0 .414.336.75.75.75h5.25a.75.75 0 00.75-.75zm-8.25 0v-.625a.75.75 0 00-.75-.75H3.25a.75.75 0 00-.75.75V7.5c0 .414.336.75.75.75H8.5a.75.75 0 00.75-.75z">
@@ -131,7 +131,7 @@
                 'flex items-center p-2 text-gray-600 rounded-lg dark:text-white hover:bg-blue-100 dark:hover:bg-gray-700': true
               }">
                 <svg
-                  class="w-6 h-6  group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                  class="w-6 h-6   dark:text-gray-400 dark:group-hover:text-white"
                   fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                   <path clip-rule="evenodd" fill-rule="evenodd"
                     d="M8.34 1.804A1 1 0 019.32 1h1.36a1 1 0 01.98.804l.295 1.473c.497.144.971.342 1.416.587l1.25-.834a1 1 0 011.262.125l.962.962a1 1 0 01.125 1.262l-.834 1.25c.245.445.443.919.587 1.416l1.473.294a1 1 0 01.804.98v1.361a1 1 0 01-.804.98l-1.473.295a6.95 6.95 0 01-.587 1.416l.834 1.25a1 1 0 01-.125 1.262l-.962.962a1 1 0 01-1.262.125l-1.25-.834a6.953 6.953 0 01-1.416.587l-.294 1.473a1 1 0 01-.98.804H9.32a1 1 0 01-.98-.804l-.295-1.473a6.957 6.957 0 01-1.416-.587l-1.25.834a1 1 0 01-1.262-.125l-.962-.962a1 1 0 01-.125-1.262l.834-1.25a6.957 6.957 0 01-.587-1.416l-1.473-.294A1 1 0 011 10.68V9.32a1 1 0 01.804-.98l1.473-.295c.144-.497.342-.971.587-1.416l-.834-1.25a1 1 0 01.125-1.262l.962-.962A1 1 0 015.38 3.03l1.25.834a6.957 6.957 0 011.416-.587l.294-1.473zM13 10a3 3 0 11-6 0 3 3 0 016 0z">
@@ -146,7 +146,7 @@
                 'flex items-center p-2 text-gray-600 rounded-lg dark:text-white hover:bg-blue-100 dark:hover:bg-gray-700': true
               }">
                 <svg
-                  class="flex-shrink-0 w-6 h-6  group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                  class="flex-shrink-0 w-6 h-6   dark:text-gray-400 dark:group-hover:text-white"
                   fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path fill-rule="evenodd"
                     d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm2 10a1 1 0 10-2 0v3a1 1 0 102 0v-3zm2-3a1 1 0 011 1v5a1 1 0 11-2 0v-5a1 1 0 011-1zm4-1a1 1 0 10-2 0v7a1 1 0 102 0V8z"
@@ -161,7 +161,7 @@
                 'flex items-center p-2 text-gray-600 rounded-lg dark:text-white hover:bg-blue-100 dark:hover:bg-gray-700': true
               }">
                 <svg
-                  class="flex-shrink-0 w-6 h-6  group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                  class="flex-shrink-0 w-6 h-6   dark:text-gray-400 dark:group-hover:text-white"
                   fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
                   <path fill-rule="evenodd"
@@ -173,7 +173,7 @@
             </li>
             <li>
               <button type="button" @click="logout"
-                class="w-full flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-blue-100 dark:hover:bg-gray-700">
+                class="w-full flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-red-100 dark:hover:bg-gray-700">
                 <svg class="w-6 h-6 text-red-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                   fill="none" viewBox="0 0 16 16">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
